d51e934b3df3c61be78a2bc86345b396e6f447f9
Abdur Razzaque <razzaque.0011@gamail.com>
2025-03-01 09:09:16 +0000

    text and update time add

---
+++ utilities/clear.js
@@ -0,0 +1,5 @@
+let clear = document.getElementById('clear');
+
+clear.addEventListener('click', function () {
+    
+})
--- utilities/dev.js
+++ utilities/dev.js
@@ -8,12 +8,20 @@ function bgColorChange() {
 
 let btn1 = document.getElementById('btn1');
 let numberDicrease = document.getElementById('numberDicrease');
-let numberIncrease = document.getElementById('numberIncrease'); 
+let numberIncrease = document.getElementById('numberIncrease');
+let fixText = document.getElementById('fixText');
+let place = document.getElementById('place');
 
 btn1.addEventListener('click', function () {
-    alert('You are select');  
+    alert('Board update successfully');  
+    
     let numberValue1 = parseInt(numberDicrease.innerText, 10);
     let numberValue2 = parseInt(numberIncrease.innerText, 10);
+    let time = new Date();  
+    let updateTime = time.toLocaleTimeString();  
+    let p = document.createElement('p');
+    p.innerText = `You have completed the task ${fixText.innerText} at ${updateTime}`;
+    place.appendChild(p);
     numberValue1 -= 1; 
     numberValue2 += 1;  
     numberDicrease.innerText = numberValue1;
