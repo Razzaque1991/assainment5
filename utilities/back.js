3f43c976639e5483faee696a7b7892f00dfa2647
Abdur Razzaque <razzaque.0011@gamail.com>
2025-03-01 11:41:08 +0000

    all ok only question paper answear let

---
+++ utilities/back.js
@@ -0,0 +1,9 @@
+let next = document.getElementById('next');
+next.addEventListener('click', function () {
+    window.location.href = 'question.html';
+});
+
+let back = document.getElementById('back');
+back.addEventListener('click', function () {
+    window.location.href = 'index.html'; 
+});
