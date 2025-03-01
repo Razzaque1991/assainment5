79b024ccd3bb94cd7ce8c94ee49de93ab751bc95
Abdur Razzaque <razzaque.0011@gamail.com>
2025-03-01 15:29:10 +0000

    all project ok

--- utilities/back.js
+++ utilities/back.js
@@ -3,7 +3,4 @@ next.addEventListener('click', function () {
     window.location.href = 'question.html';
 });
 
-let back = document.getElementById('back');
-back.addEventListener('click', function () {
-    window.location.href = 'index.html'; 
-});
+  
